c897908aeb90d6e41f3be781cd9cf7abb0aad9f4
MRYuri <pal.dominik@petrik.hu>
2026-02-23 14:40:23 +0100

    Frissitve 02.23 14:40

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+namespace Huto_feladat;
+
+class Program
+{
+    private static List<string> tartalom = new List<string>();
+
+    static void Listazas()
+    {
+        Console.WriteLine("A hűtő tartalma:");
+        foreach (var item in tartalom)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static void Feltoltes()
+    {
+        string valasz;
+        do
+        {
+            Console.WriteLine("Milyen ételt szeretne hozzáadni");
+            valasz = Console.ReadLine();
+            if (valasz != "")
+            {
+                tartalom.Add(valasz);
+            }
+        } while (valasz != "");
+    }
+
+    static void FaljbaIras()
+    {
+        StreamReader sr = new StreamReader("Tartalom.txt");
+    }
+    
+    static void Main(string[] args)
+    {
+        char valasz;
+        do
+        {
+            Console.WriteLine("|--------------------------------------------------------|");
+            Console.WriteLine("Menu: \n\t1. listázás: \n\t2. Étel berakása a hűtőbe \n\t3. Étel kivétele \n\t4. Hütő takaritása \n\t5. Exportálás \n\t6. Kilépés");
+            valasz = Console.ReadLine()[0];
+            switch (valasz)
+            {
+                case '1':
+                    Listazas();
+                    break;
+                case '2':
+                    Feltoltes();
+                    break;
+                case '3':
+                    
+                    break;
+                case '4':
+                    
+                    break;
+                case '5':
+                    
+                    break;
+                case '6':
+                    Console.WriteLine("Viszlát");
+                    break;
+            }
+            Console.WriteLine("|--------------------------------------------------------|");
+        } while (valasz != '6');
+
+
+    }
+}
